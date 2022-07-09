d43d300f1618c58b0be13777f600f14406c6307b
Santosh T <bt.santosh01@gmail.com>
2022-07-09 08:09:19 +0100

    separated the spec file

--- event_handler.rb
+++ event_handler.rb
@@ -1,5 +1,3 @@
-require "rspec/autorun"
-
 class EventHandler
     class << self
         @@handlers = []
@@ -60,62 +58,5 @@ class EventHandler
                 "Invalid broadcast event"
             end
         end
-    end
-end
-
-
-describe EventHandler, ".subscribe" do
-    it "it returns single stored event handler in an array" do
-        subscribed_events = EventHandler.subscribe {|x| x}
-        expect(subscribed_events.size).to eq(1) 
-    end
-
-    it "it returns multiple event handlers in an array" do
-        new_subscribed_events = EventHandler.subscribe {|x, y| x + 4 * 250 + y}
-        expect(new_subscribed_events.size).to eq(2) 
-    end
-
-    it "it returns handler already exits" do
-        subscribed_events = EventHandler.subscribe {|x| x}
-        expect(subscribed_events).to eq("Sorry, handler already exists.")
-    end
-
-    it "returns No block given" do
-        subscribed_event = EventHandler.subscribe
-        expect(subscribed_event).to eq("No block given")
-    end
-end
-
-describe EventHandler, ".unsubscribe" do
-    it "it removes the matching block from the stored handlers" do
-        subscribed_events = EventHandler.unsubscribe {|x| x}
-        expect(subscribed_events.size).to eq(1) 
-    end
-
-    it "returns No block given" do
-        subscribed_events = EventHandler.unsubscribe
-        expect(subscribed_events).to eq("No block given")
-    end
-end
-
-describe EventHandler, ".broadcast" do
-    it "it calls all the existing event handlers with arbitrary number of arguments (1,2,3,4)" do
-        broadcast_handler_01 = EventHandler.broadcast(1,2,3,4)
-        expect(broadcast_handler_01).to eq([1003]) 
-    end
-
-    it "it calls all the existing event handlers with arbitrary number of arguments (3, 8)" do
-        broadcast_handler_02 = EventHandler.broadcast(150, 240)
-        expect(broadcast_handler_02).to eq([1390])
-    end
-
-    it "it calls all the existing event handlers with no arguments ()" do
-        broadcast_handler_03 = EventHandler.broadcast()
-        expect(broadcast_handler_03).to eq("Arguments required")
-    end
-
-    it "it returns invalid broadcast event" do
-        broadcast_handler_04 = EventHandler.broadcast("John")
-        expect(broadcast_handler_04).to eq(["Invalid broadcast event"])
     end
 end
